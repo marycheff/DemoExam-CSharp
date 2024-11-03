a3aaec8e4d9a1395bb59566ce0b4a20c0dfa3da5
Maksim Marychev <marycheff17@gmail.com>
2024-11-03 18:31:08 +0400

    MainForm: MaximizeBox, MinimizeBox

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -61,6 +61,8 @@
             this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Партнеры";
